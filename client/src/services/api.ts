4a93f182331c3523bf2a5e80676723efcb5531c5
Polycarpe MAKOMBO <42547869+polymak@users.noreply.github.com>
2026-03-08 04:24:07 +0100

    update api url

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-export const API_URL = "https://blog-backend-kf3i.onrender.com/api";
-export const BASE_UPLOAD_URL = "https://blog-backend-kf3i.onrender.com";
+export const API_URL =
+  "https://blog-backend-kf3i.onrender.com/api";
+
+export const BASE_UPLOAD_URL =
+  "https://blog-backend-kf3i.onrender.com";
 
 const API = axios.create({
   baseURL: API_URL,
